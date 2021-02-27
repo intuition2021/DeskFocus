622c75fa9ddc655695158e6296b2bc6a023391fc
hua-lun <chuahualun@gmail.com>
2021-02-27 17:49:52 +0800

    Add code for website blocker

--- blocker.py
+++ blocker.py
@@ -1,6 +1,7 @@
 import ast
 import time
 from datetime import datetime as dt
+from datetimerange import DateTimeRange
 
 file = open("sysInfo.txt")
 sysInfo = ast.literal_eval(file.read())
@@ -8,19 +9,47 @@ sysInfo = ast.literal_eval(file.read())
 daysNeeded = sysInfo["days"]
 timeStamp = sysInfo["time"]
 
-#timeStrStart = sysInfo["time"][0]
-#timeObjStart = dt.strptime(timeStrStart, '%H:%M')
-
 redirect = "127.0.0.1"
 today = dt.today().date().strftime('%A')
 
-while today in daysNeeded:
-    with open(sysInfo["system"], 'r+') as file:
-        content = file.read()
-    for website in sysInfo["blockSites"]:
-        if website in content:
-            pass
-        else:
-            file.write(redirect + " " + website + "\n")
+date = dt.now()
+
+startTime = ""
+endTime = ""
+time_range = ""
+
+timeStrStart = sysInfo["time"][today][0]
+timeStrEnd = sysInfo["time"][today][1]
+
+if today in daysNeeded:
+    day = int(date.strftime("%d"))
+    month = int(date.strftime("%m"))
+    year = int(date.strftime("%Y"))
+
+    timeObjStart = dt.strptime(timeStrStart, '%H:%M')
+    timeObjEnd = dt.strptime(timeStrEnd, '%H:%M')
+
+    startTime = timeObjStart.replace(year=year, month=month, day=day)
+    endTime = timeObjEnd.replace(year=year, month=month, day=day)
+
+    time_range = DateTimeRange(startTime, endTime)
+
+myhost = sysInfo["system"]
+
+while True:
+    while today in daysNeeded and dt.now() in time_range:
+        with open(myhost, 'r+') as myhostfile:
+            hosts = myhostfile.read()
+            for site in sysInfo["blockSites"]:
+                if site not in hosts:
+                    myhostfile.write(redirect + ' ' + site + '\n')
+
+    with open(myhost, 'r+') as myhostfile:
+        hosts = myhostfile.readlines()
+        myhostfile.seek(0)
+        for host in hosts:
+            if not any(site in host for site in sysInfo["blockSites"]):
+                myhostfile.write(host)
+        myhostfile.truncate()
 
     time.sleep(10)
