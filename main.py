5329727aaeeef9d8310414461b1e0a92878b593b
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-02-27 12:06:52 +0800

    Add imports for pyqt5'

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+# -*- coding: utf-8 -*-
+
+import ctypes
+
+import logging
+import sys
+from threading import Thread
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QColorDialog, QSizePolicy, QMessageBox, QLabel
